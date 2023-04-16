0ccd0c7184df8aaf7d8ab300a05f674575becb5a
Arthur <61556875+Absolute-Arthur@users.noreply.github.com>
2023-04-16 16:46:07 +0200

    Add states input to console

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
@@ -22,10 +25,31 @@ class FiniteAutomaton {
 }
 
 class State {
-	public String name;
+	private String name;
 
-	public State(String name){
+	public State(String name) {
 		this.name = name;
+	}
+
+	public String name() {
+		return name;
+	}
+
+	public String toString() {
+		return name();
+	}
+}
+
+class StatesPrinter {
+	public static String print(Set<State> states) {
+		String result = "[";
+		Iterator<State> statesIterator = states.iterator();
+		if(statesIterator.hasNext()) result = result + statesIterator.next().name();
+		while(statesIterator.hasNext()) {
+			result = result + ", " + statesIterator.next().name();
+		}
+		result = result + "]";
+		return result;
 	}
 }
 
@@ -39,8 +63,35 @@ class Letter {
 
 class Console {
 	public static void console() {
-		System.out.println("Bonjour !");
-		
+		System.out.println("""
+			--------
+			Welcome to the finite automaton emulator !
+			I will ask, in order 
+			1) The name of the states
+			2) The letters of the alphabet
+			3) The transitions
+			""");
+			Scanner input = new Scanner(System.in);
+			String currentStringInput;
+			Set<State> states = new HashSet<State>();
+
+			do {
+				System.out.print("State name : ");
+				currentStringInput = input.nextLine();
+				if(currentStringInput == "") System.out.println("You need to have at least one state");
+			} while (currentStringInput == "");
+
+			states.add(new State(currentStringInput));
+
+			do {
+				System.out.println("Current states : " + StatesPrinter.print(states));
+				System.out.print("State name : ");
+				currentStringInput = input.nextLine();
+				if(currentStringInput != "") states.add(new State(currentStringInput));
+			} while(currentStringInput != "");
+			
+			System.out.println("The states will be : " + StatesPrinter.print(states));
+			input.close();
 	}
 }
 
